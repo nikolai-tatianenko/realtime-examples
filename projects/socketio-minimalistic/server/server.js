83c4f318c7b4555d10906dcba0385a08c4e509a2
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2022-05-03 19:44:45 +0000

    Minimalistic | Added loading history | Server side

--- projects/socketio-minimalistic/server/server.js
+++ projects/socketio-minimalistic/server/server.js
@@ -3,13 +3,18 @@ const http = require('http').createServer();
 const io = require('socket.io')(http, {
   cors: { origin: '*' },
 });
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  // const messages = [];
-  socket.on('message', ({ username="<unknown username>", message ="<empty message>"}) => {
-    // messages.push(message);
 
-    io.emit('message', `${username} : "${message}"`);
+const chatHistory = [];
+
+io.on('connection', (socket) => {
+  // Add a new "loadHistory" event to send the chat history to the client
+  socket.emit('loadHistory', chatHistory);
+  socket.on('message', ({ username="<unknown username>", message ="<empty message>"}) => {
+    // Add the new message to the chat history
+    chatHistory.push(`${username}: ${message}`);
+
+    // Emit the new message to all connected clients
+    io.emit('message', `${username}: ${message}`);
   });
 });
 
