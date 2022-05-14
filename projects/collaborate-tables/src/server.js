6ec63ddf70faca443f1c3e9399d3c62ab693a784
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2022-05-14 01:08:45 +0000

    feat: vanilla table - create server

--- projects/collaborate-tables/src/server.js
+++ projects/collaborate-tables/src/server.js
@@ -13,3 +13,33 @@ const index = fs.readFileSync('public/index.html', 'utf8');
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 8000;
 
+/**
+ * @brief Create an HTTP server to handle requests.
+ *
+ * This server serves static files from the 'public' directory.
+ */
+const server = http.createServer((req, res) => {
+  const filePath = req.url === '/' ? 'index.html' : req.url; // Determine the file path based on the request URL
+  const fileExt = path.extname(filePath); // Get the file extension from the file path
+  const contentType = getContentType(fileExt); // Get the content type based on the file extension
+
+  fs.readFile(path.join(__dirname, '../public', filePath), (err, content) => {
+    console.log('file', err, content);
+    if (err) {
+      if (err.code === 'ENOENT') {
+        // File not found
+        res.writeHead(404);
+        res.end('404 - File Not Found');
+      } else {
+        // Server error
+        res.writeHead(500);
+        res.end('500 - Internal Server Error');
+      }
+    } else {
+      // Success
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf8');
+    }
+  });
+});
+
